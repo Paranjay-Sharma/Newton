4291905e6e10b7b88b494fb84e8871dc40e7ea4d
Paranjay Sharma <76864845+Paranjay-Sharma@users.noreply.github.com>
2021-02-08 15:17:33 +0530

    Add files via upload

---
+++ pro 27/Bob.js
@@ -0,0 +1,25 @@
+
+class Bob {
+    constructor(x,y,radius) {
+        var options = {
+            isStatic: false,
+            restitution: 1.3,
+            friction:0.8,
+            density:0.9
+        }
+        
+        this.body = Bodies.circle(x,y,radius, options);
+        this.radius = radius
+
+        World.add(world, this.body);
+    }
+    display(){
+        
+        ellipseMode(RADIUS);
+
+        fill("magenta");
+
+        circle(this.body.position.x, this.body.position.y, this.radius);
+
+    }       
+};
